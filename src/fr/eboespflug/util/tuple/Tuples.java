0f398e0164393f0c0b2cec04b06688af3f4c1ade
Etienne Boespflug <etienne.boespflug@gmail.com>
2017-03-01 01:30:30 +0100

    Added Tuple interface.

---
+++ src/fr/eboespflug/util/tuple/Tuples.java
@@ -0,0 +1,13 @@
+package fr.eboespflug.util.tuple;
+
+/**
+ * /TMP/
+ *
+ * @author Etienne Boespflug
+ * @since 1.0
+ *
+ * Created by Etienne Boespflug on 2/12/2017.
+ */
+@SuppressWarnings("WeakerAccess")
+public class Tuples {
+}
