3f0b7fe1aa43ca01b25d7eb31f9b9555a6be4d2b
Etienne Boespflug <etienne.boespflug@gmail.com>
2017-03-02 12:48:21 +0100

    Added create methods in Tuples.

--- src/fr/eboespflug/util/tuple/Tuples.java
+++ src/fr/eboespflug/util/tuple/Tuples.java
@@ -10,4 +10,16 @@ package fr.eboespflug.util.tuple;
  */
 @SuppressWarnings("WeakerAccess")
 public class Tuples {
+
+    public static <_Type1, _Type2> Pair create(_Type1 a, _Type2 b) {
+        return new Pair<>(a, b);
+    }
+
+    public static <_Type1, _Type2, _Type3> Triplet create(_Type1 a, _Type2 b, _Type3 c) {
+        return new Triplet<>(a, b, c);
+    }
+
+    public static <_Type1, _Type2, _Type3, _Type4> Quadruple create(_Type1 a, _Type2 b, _Type3 c, _Type4 d) {
+        return new Quadruple<>(a, b, c, d);
+    }
 }
